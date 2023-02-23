4a4a3cdd83fedd2dbf805422b71d63da71cf804b
Hari-01 <f20190564@pilani.bits-pilani.ac.in>
2023-02-23 13:53:31 +0530

    first set init

---
+++ first/First.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+char dataReadFromFile[100];
+
+int main (){
+
+    char data[130][100];
+    char nonTerminals[120][20];
+
+    struct node {
+        int char[20];                        //Data of the node
+        struct node *nextPtr;           //Address of the next node
+    }*stNode;
+
+    void createNodeList(int n); // function to create the list
+    void displayList();         // function to display the list
+
+
+    FILE *fp; // storing grammar contents from file to 2d array
+    fp = fopen("Grammar.txt", "r");
+
+    if (fp == NULL)
+    {
+        printf("Failure to open Grammar.txt");
+    }
+    else
+    {
+        // printf("Grammar.txt is opened.");
+        int count = 0;   
+        // printf("%d",count);    
+        // while( fgets (dataReadFromFile, 90, fp) != NULL )
+        // {
+        //     strcpy(data[count],dataReadFromFile);
+        //     count++;
+        //     // Print the dataToBeRead
+        //     // printf( "%s" , dataReadFromFile) ;
+        // }
+
+        fgets (dataReadFromFile, 90, fp);
+        char * token = strtok(dataReadFromFile, " ");
+         // loop through the string to extract all other tokens
+        while( token != NULL ) {
+            printf( " %s\n", token ); //printing each token
+            token = strtok(NULL, " ");
+        }
+
+        struct node *forwardNode, *tempNode;
+        startNode = struct 
+        int finalCountOfGrammar = count;
+        // printing final count
+        // printf("%d",finalCountOfGrammar);
+
+        // Closing the file using fclose()
+        fclose(fp) ;
+
+        // for(int count = 0; count < finalCountOfGrammar;count++){
+        //     for(int characterNumber = 0; data[count][characterNumber] != "→"; characterNumber++){
+
+        //     }
+        // }
+
+
+    }
+}
+
