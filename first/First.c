f889e3f16313cdfb5c6f004e2404661fd2012bb0
ash <65379684+Suzukazole@users.noreply.github.com>
2023-02-23 16:05:49 +0530

    start first set

--- first/First.c
+++ first/First.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define GRAMMAR_SIZE 120
+
 #define MAX_TOKEN_SIZE 30
 #define MAX_LINE_SIZE 100
-char dataReadFromFile[100];
+char dataReadFromFile[MAX_LINE_SIZE];
 
 //open the grammar 
 FILE* openGrammar(char* fileName){
@@ -66,18 +66,88 @@ void printList(ruleNode* head){
     printf("\n");
 }
 
+
+// function to check if the string is a non terminal
+int isNonTerminal(char* token){
+    if(token[0] == '<'){
+        return 1;
+    }
+    return 0;
+}
+
+// obtain the non terminals from the grammar
+void getNonTerminals(ruleNode* head, char* nonTerminals){
+    ruleNode* temp = head;
+    while(temp != NULL){
+        if(isNonTerminal(temp->grammarEntry)){
+            strcat(nonTerminals, temp->grammarEntry);
+            strcat(nonTerminals, " ");
+        }
+        temp = temp->nextPtr;
+    }
+    //print the non terminals
+    printf("%s\n", nonTerminals);
+}
+
+
+void getFirst(char* nonTerminal, ruleNode* headArray[], int grammarSize) {
+    // TODO: CORRECT THIS!
+    printf("First(%s) = ", nonTerminal);
+    for(int i = 0; i < grammarSize; i++){
+        if(strcmp(nonTerminal, headArray[i]->grammarEntry) == 0){
+            ruleNode* temp = headArray[i]->nextPtr;
+            while(temp != NULL){
+                if(isNonTerminal(temp->grammarEntry)){
+                    getFirst(temp->grammarEntry, headArray, grammarSize);
+                }
+                else{
+                    printf("%s ", temp->grammarEntry);
+                    break;
+                }
+                temp = temp->nextPtr;
+            }
+        }
+
+    }
+}
+
+
+
 int main(){
     FILE* fp = openGrammar("Grammar.txt");
-    int count = 0;
+    int grammarSize = 0;
+    // find the size of the grammar
+    while(fgets(dataReadFromFile, MAX_LINE_SIZE, fp) != NULL){
+        grammarSize++;
+    }
+    // move the file pointer to the beginning of the file
+    fseek(fp, 0, SEEK_SET);
+
     // create an array of heads
-    ruleNode* headArray[GRAMMAR_SIZE];
+    ruleNode* headArray[grammarSize];
+    int count = 0;
     while(fgets(dataReadFromFile, MAX_LINE_SIZE, fp) != NULL){
         ruleNode* head = createRuleList(dataReadFromFile);
         headArray[count] = head;
         count++;
-    
     }
-    printList(headArray[64]);
+    
+    char nonTerminals[grammarSize][MAX_TOKEN_SIZE];
+    for(int i = 0; i < grammarSize; i++){
+        strcpy(nonTerminals[i], headArray[i]->grammarEntry);
+    }
+    // remove duplicates from the nonTerminals array
+    for(int i = 0; i < grammarSize; i++){
+        for(int j = i + 1; j < grammarSize; j++){
+            if(strcmp(nonTerminals[i], nonTerminals[j]) == 0){
+                strcpy(nonTerminals[j], "");
+            }
+        }
+    }
+    getFirst("<program>", headArray, grammarSize);
+
+
+   
     return 0;
 }
 
