2a278daa3f96d885154078c3fb6d2725c478aeef
ash <65379684+Suzukazole@users.noreply.github.com>
2023-02-23 15:07:41 +0530

    made the structure for the rules neater

--- first/First.c
+++ first/First.c
@@ -1,69 +1,110 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
+#define GRAMMAR_SIZE 120
+#define MAX_TOKEN_SIZE 30
+#define MAX_LINE_SIZE 100
 char dataReadFromFile[100];
 
-int main (){
-
-    char data[130][100];
-    char nonTerminals[120][20];
-
-    void createNodeList(int n); // function to create the list
-    void displayList();         // function to display the list
-
-
-    FILE *fp; // storing grammar contents from file to 2d array
-    fp = fopen("Grammar.txt", "r");
-
+//open the grammar 
+FILE* openGrammar(char* fileName){
+    FILE* fp = fopen(fileName, "r");
     if (fp == NULL)
     {
         printf("Failure to open Grammar.txt");
     }
     else
     {
-        // printf("Grammar.txt is opened.");
-        int count = 0;   
-        // printf("%d",count);    
-        // while( fgets (dataReadFromFile, 90, fp) != NULL )
-        // {
-        //     strcpy(data[count],dataReadFromFile);
-        //     count++;
-        //     // Print the dataToBeRead
-        //     // printf( "%s" , dataReadFromFile) ;
-        // }
-
-        fgets (dataReadFromFile, 90, fp);
-        char * token = strtok(dataReadFromFile, " ");
-        struct node {
-            int nodeToken[20];                        //Data of the node
-            struct node *nextPtr;           //Address of the next node
-        }*startNode;
-         // loop through the string to extract all other tokens
-        while( token != NULL ) {
-            printf( " %s\n", token ); //printing each token
-            
-            token = strtok(NULL, " ");
-        }
-
-        struct node *forwardNode, *tempNode;
-        startNode = (struct node *)malloc(sizeof(struct node));
-        startNode->nodeToken = token;
-        startNode ->nextPtr = NULL;
-        int finalCountOfGrammar = count;
-        // printing final count
-        // printf("%d",finalCountOfGrammar);
-
-        // Closing the file using fclose()
-        fclose(fp) ;
-
-        // for(int count = 0; count < finalCountOfGrammar;count++){
-        //     for(int characterNumber = 0; data[count][characterNumber] != "→"; characterNumber++){
-
-        //     }
-        // }
-
-
+        printf("Grammar.txt is opened.");
     }
+    return fp;
 }
+
+// create a structure to store the entries of the grammar
+typedef struct ruleNode {
+    // stores the entry as a string and contains a pointer to the next node in a rule
+    char grammarEntry[MAX_TOKEN_SIZE];
+    struct ruleNode *nextPtr;
+
+} ruleNode;
+
+// function to add a node to the rule
+void addNode(ruleNode* head, char* grammarEntry){
+    ruleNode* newNode = (ruleNode*)malloc(sizeof(ruleNode));
+    strcpy(newNode->grammarEntry, grammarEntry);
+    newNode->nextPtr = NULL;
+    ruleNode* temp = head;
+    while(temp->nextPtr != NULL){
+        temp = temp->nextPtr;
+    }
+    temp->nextPtr = newNode;
+}
+
+//function to create a linked list for each rule
+ruleNode* createRuleList(char* dataReadFromFile){
+    char * token = strtok(dataReadFromFile, " ");
+    ruleNode* head = (ruleNode*)malloc(sizeof(ruleNode));
+    strcpy(head->grammarEntry, token);
+    head->nextPtr = NULL;
+    token = strtok(NULL, " ");
+    while( token != NULL ) {
+        addNode(head, token);
+        token = strtok(NULL, " ");
+    }
+    return head;
+}
+
+// function to print the linked list
+void printList(ruleNode* head){
+    ruleNode* temp = head;
+    while(temp != NULL){
+        printf("%s ", temp->grammarEntry);
+        temp = temp->nextPtr;
+    }
+    printf("\n");
+}
+
+int main(){
+    FILE* fp = openGrammar("Grammar.txt");
+    int count = 0;
+    // create an array of heads
+    ruleNode* headArray[GRAMMAR_SIZE];
+    while(fgets(dataReadFromFile, MAX_LINE_SIZE, fp) != NULL){
+        ruleNode* head = createRuleList(dataReadFromFile);
+        headArray[count] = head;
+        count++;
+    
+    }
+    printList(headArray[64]);
+    return 0;
+}
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
