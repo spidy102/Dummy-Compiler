b0dec492b5f9b9770823aada8816126de358df26
spidy102 <shauryamarwah02@gmail.com>
2023-03-01 12:46:56 +0530

    fix time computation option

--- driver.c
+++ driver.c
@@ -8,7 +8,8 @@
 #include "parserDef.h"
 #include <time.h>
 
-void displayImplementationStatus(){
+void displayImplementationStatus()
+{
   printf("____________________________________________________________\n");
   printf("Implementation Status:\n\n");
   printf("a) FIRST and FOLLOW Sets automated.\n");
@@ -18,7 +19,8 @@ void displayImplementationStatus(){
   printf("____________________________________________________________\n");
 }
 
-void displayMenu(){
+void displayMenu()
+{
 
   printf("0. Exit\n");
   printf("1. Remove comments and display the comment free source code.\n");
@@ -27,35 +29,43 @@ void displayMenu(){
   printf("4. Display time taken to parse the source code.\n");
 
   printf("Enter your choice [0-4]: ");
-
 }
 
-void displayFile(FILE* fp){
+void displayFile(FILE *fp)
+{
   printf("Displaying the file...\n");
   char c;
   fseek(fp, 0, SEEK_SET);
-  while((c = fgetc(fp)) != EOF){
+  while ((c = fgetc(fp)) != EOF)
+  {
     printf("%c", c);
   }
   printf("\n");
 }
 
-int takeInput(){
+int takeInput()
+{
   int choice;
-  while(1){
-    if(scanf("%d", &choice) == 1) break;
-    else{
+  while (1)
+  {
+    if (scanf("%d", &choice) == 1)
+      break;
+    else
+    {
       printf("Invalid choice. Please try again.\n");
       printf("Enter your choice [0-4]: ");
-      while(getchar() != '\n');
+      while (getchar() != '\n')
+        ;
     }
   }
   return choice;
 }
 
-FILE* openfile(char *filename, char *mode){
+FILE *openfile(char *filename, char *mode)
+{
   FILE *fp = fopen(filename, mode);
-  if (fp == NULL){
+  if (fp == NULL)
+  {
     printf("Error opening file. Exiting.\n");
     exit(1);
   }
@@ -64,42 +74,46 @@ FILE* openfile(char *filename, char *mode){
 
 char *EnumToString(token_names nt)
 {
-    int i = 0;
-    FILE *fp = fopen("tokens.txt", "r");
-    char *buffer = malloc(sizeof(char) * 100);
-    while (fgets(buffer, 100, fp) != NULL)
-    {
-        buffer[strlen(buffer) - 3] = '\0';
-        // printf("%s\n", buffer);
+  int i = 0;
+  FILE *fp = fopen("tokens.txt", "r");
+  char *buffer = malloc(sizeof(char) * 100);
+  while (fgets(buffer, 100, fp) != NULL)
+  {
+    buffer[strlen(buffer) - 3] = '\0';
+    // printf("%s\n", buffer);
 
-        if (nt == i)
-            return buffer;
-        i++;
-    }
+    if (nt == i)
+      return buffer;
+    i++;
+  }
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
-  //expect 2 strings and an integer as arguments
-  if (argc != 4){
+  // expect 2 strings and an integer as arguments
+  if (argc != 4)
+  {
     printf("Invalid number of arguments. Expected 3 arguments. Exiting.\n");
     exit(1);
   }
-  //ensure that the size of buffer is a valid integer
-  for (int i = 0; i < strlen(argv[3]); i++){
-    if (argv[3][i] < '0' || argv[3][i] > '9'){
+  // ensure that the size of buffer is a valid integer
+  for (int i = 0; i < strlen(argv[3]); i++)
+  {
+    if (argv[3][i] < '0' || argv[3][i] > '9')
+    {
       printf("Invalid size of buffer. Expected an integer. Exiting.\n");
       exit(1);
     }
   }
 
-  //parse the arguments
+  // parse the arguments
   char *testcase = argv[1];
   char *parsetreeOutFile = argv[2];
   int size_of_buffer = atoi(argv[3]);
 
-  //construct the twinbuffer
-  twinbuffer* twin_buf;
+  // construct the twinbuffer
+  twinbuffer *twin_buf;
   twin_buf = twinbuffer_init(openfile(testcase, "r"), size_of_buffer);
   hashtable ht = initHashtable();
   populate_hashtable(&ht);
@@ -108,68 +122,91 @@ int main(int argc, char *argv[]){
   displayMenu();
   int choice = takeInput();
 
-
   clock_t start_time, end_time;
   double total_CPU_time, total_CPU_time_in_seconds;
-  
-  while(1){
 
-    if (choice == 0) exit(0);
-    else if (choice == 1){
-      FILE* fp1 = removeComments(twin_buf, testcase);
+  while (1)
+  {
+
+    if (choice == 0)
+      exit(0);
+    else if (choice == 1)
+    {
+      FILE *fp1 = removeComments(twin_buf, testcase);
       displayFile(fp1);
       fclose(fp1);
       break;
     }
-    else if (choice == 2){
+    else if (choice == 2)
+    {
       line_num = 1;
-      token* tk = getNextToken(ht,twin_buf);
-      while(tk!=NULL) {
-        if (tk->token==NUM) {
-          printf("%d %d %s\n",tk->integer,tk->line_num, EnumToString(tk->token));
-          tk = getNextToken(ht,twin_buf);
+      token *tk = getNextToken(ht, twin_buf);
+      while (tk != NULL)
+      {
+        if (tk->token == NUM)
+        {
+          printf("%d %d %s\n", tk->integer, tk->line_num, EnumToString(tk->token));
+          tk = getNextToken(ht, twin_buf);
           continue;
         }
-        else if (tk->token==RNUM) {
-          printf("%f %d %s\n",tk->rnum,tk->line_num,EnumToString(tk->token));
-          tk = getNextToken(ht,twin_buf);
+        else if (tk->token == RNUM)
+        {
+          printf("%f %d %s\n", tk->rnum, tk->line_num, EnumToString(tk->token));
+          tk = getNextToken(ht, twin_buf);
           continue;
         }
-        printf("%s %d %s\n",tk->str,tk->line_num,EnumToString(tk->token));
-        tk = getNextToken(ht,twin_buf);
+        printf("%s %d %s\n", tk->str, tk->line_num, EnumToString(tk->token));
+        tk = getNextToken(ht, twin_buf);
       }
       break;
     }
-    else if (choice == 3){
-      FILE* fg = openfile("Grammar.txt", "r");
+    else if (choice == 3)
+    {
+      FILE *fg = openfile("Grammar.txt", "r");
 
-      start_time = clock();
+      // start_time = clock();
       // invoke your lexer and parser here
       fill_grammar(fg);
       populateParseTable();
-      treenode* root = parseInputSourceCode(openfile(testcase, "r+"), twin_buf, ht);
+      treenode *root = parseInputSourceCode(openfile(testcase, "r+"), twin_buf, ht);
       printParseTree(root, openfile(parsetreeOutFile, "w"));
 
-      end_time = clock();
-      total_CPU_time = (double) (end_time - start_time);
-      total_CPU_time_in_seconds = total_CPU_time / CLOCKS_PER_SEC;
+      // end_time = clock();
+      // total_CPU_time = (double)(end_time - start_time);
+      // total_CPU_time_in_seconds = total_CPU_time / CLOCKS_PER_SEC;
 
       fclose(fg);
 
       break;
     }
-    else if (choice == 4){
+    else if (choice == 4)
+    {
       printf("You chose 4\n");
+      FILE *fg = openfile("Grammar.txt", "r");
+
+      start_time = clock();
+      // invoke your lexer and parser here
+      fill_grammar(fg);
+      populateParseTable();
+      treenode *root = parseInputSourceCode(openfile(testcase, "r+"), twin_buf, ht);
+      printParseTree(root, openfile(parsetreeOutFile, "w"));
+
+      end_time = clock();
+      total_CPU_time = (double)(end_time - start_time);
+      total_CPU_time_in_seconds = total_CPU_time / CLOCKS_PER_SEC;
+
+      fclose(fg);
+
       printf("Time taken to parse the source code: %lf seconds\n", total_CPU_time_in_seconds);
       break;
     }
-    else{
+    else
+    {
       printf("Invalid choice. Please try again.\n");
       printf("Enter your choice [0-4]: ");
       choice = takeInput();
-    } 
+    }
   }
 
   return 0;
-
 }
